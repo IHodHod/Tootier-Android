7cc5b8ef49465be5149e8ef5dd68331f98a92673
javadwest <85465932+javadwest@users.noreply.github.com>
2022-05-11 22:58:27 +0430

    Splash

---
+++ app/src/main/java/com/hodhodo/app/tootier/splash_activity.kt
@@ -0,0 +1,12 @@
+package com.hodhodo.app.tootier
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splash_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+}
